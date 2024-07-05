c6ff08f5129fdf89d7beb1bdc280be6af9b521a5
Matias Nahuel Améndola <soporte.esolutions@gmail.com>
2024-07-05 12:36:03 -0300

    fix: multiple

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,25 +1,18 @@
-/** @type {import("@types/prettier").Options} */
-// Extracted from `webdev-configs` (https://github.com/JulianCataldo/web-garden/tree/develop/configs)
-
+/** @type {import("prettier").Config} */
 module.exports = {
-	printWidth: 80,
-	semi: true,
-	singleQuote: true,
-	tabWidth: 2,
-	trailingComma: 'all',
-
-	// See: https://github.com/prettier/prettier/issues/7475
-	useTabs: true,
-
-	plugins: ['./node_modules/prettier-plugin-astro'],
-	overrides: [
-		{
-			files: '*.astro',
-			semi: true,
-			options: {
-				parser: 'astro',
-				printWidth: 80,
-			},
-		},
-	],
-};
+  // i am just using the standard config, change if you need something else
+  ...require('prettier-config-standard'),
+  plugins: [
+    require.resolve('@trivago/prettier-plugin-sort-imports'),
+    require.resolve('prettier-plugin-astro'),
+    require.resolve('prettier-plugin-tailwindcss')
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ]
+}
